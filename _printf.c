1c73376228735bb1175cd3af839ef5933b19609a
Guta Te <39800570+GUUTA@users.noreply.github.com>
2022-07-13 13:28:00 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -36,8 +36,7 @@ else if (format[i] == 'r')
 chars_printed += print_reverse(va_arg(args, char *));
 else if (format[i] == 'R')
 chars_printed += print_rot13(va_arg(args, char *));
-else if (format[i] == 'o' || format[i] == 'u' ||
-format[i] == 'x' || format[i] == 'X')
+else if (format[i] == 'o' || format[i] == 'u' ||format[i] == 'x' || format[i] == 'X')
 chars_printed += print_odh(format[i], (unsigned int)va_arg(args, int));
 else if (format[i] == 'S')
 chars_printed += print_S(va_arg(args, char *));
